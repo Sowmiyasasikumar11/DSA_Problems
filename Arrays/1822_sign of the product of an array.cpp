88cd729777f7a4caa3b484f287a140a49e2f3eea
Sowmiyasasikumar11 <sowmiya110307@gmail.com>
2026-06-10 20:24:39 +0530

    Solved LeetCode 1822 Sign of the Product of an Array

---
+++ Arrays/1822_sign of the product of an array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int negative_count=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==0)
+              return 0;
+            if(nums[i]<0)
+            {
+                negative_count++;
+            }
+        }
+        if(negative_count%2==0)
+        {
+            return 1;
+        }
+        return -1;
+    }
+};
